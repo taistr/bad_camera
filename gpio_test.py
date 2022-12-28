fc29cc47b882b35e177898e72c3cfb6a85a4ca3c
Tyson Truong <tystruong@gmail.com>
2022-12-28 14:13:48 +1100

    Added button code

--- gpio_test.py
+++ gpio_test.py
@@ -7,13 +7,22 @@ import RPi.GPIO as GPIO
 
 GPIO_LED = 4
 
-if __name__ == "__main__":
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(GPIO_LED, GPIO.OUT, initial=GPIO.LOW)
-
-    GPIO.output(GPIO_LED, GPIO.HIGH)
-    time.sleep(3)
-    GPIO.output(GPIO_LED, GPIO.LOW)
-    
+def signal_handler(sig, frame):
     GPIO.cleanup()
     sys.exit(0)
+
+def button_callback(channel):
+    print("Button pressed!")
+
+if __name__ == "__main__":
+    #Set up pin layout mode
+    GPIO.setmode(GPIO.BCM)
+
+    #Set up pin modes
+    GPIO.setup(GPIO_LED, GPIO.OUT, pull_up_down = GPIO.PUD_UP)
+
+    GPIO.add_event_detect(BUTTON_GPIO, GPIO.FALLING, callback=button_callback, bouncetime=100)
+
+    signal.signal(signal.SIGINT, signal_handler)
+    signal.pause()
+    
