579c8ca0999ee57a0edbfd19e770d6927ee840da
Tyson Truong <tystruong@gmail.com>
2022-12-28 13:15:44 +1100

    code to test GPIO with an LED

--- gpio_test.py
+++ gpio_test.py
@@ -1,1 +1,16 @@
 #!/usr/bin/env python3
+
+import signal
+import sys
+import time
+import RPi.GPIO as GPIO
+
+GPIO_LED = 4
+
+if __name__ == "__main__":
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(GPIO_LED, GPIO.OUT, initial=GPIO.LOW)
+
+    GPIO.output(GPIO_LED, GPIO.HIGH)
+    time.sleep(3)
+    GPIO.output(GPIO_LED, GPIO.LOW)
